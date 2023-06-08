8d70add30caf8b57abea0b3bfea97a5627b9e6e8
bignetbrands <135934264+bignetbrands@users.noreply.github.com>
2023-06-07 23:51:03 -0700

    Update entry-header.js

--- components/entry-header.js
+++ components/entry-header.js
@@ -1,4 +1,5 @@
 import style from "./entry-header.module.css";
+import style from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
 
 export default function EntryHeader({ title, date, author }) {
   return (
