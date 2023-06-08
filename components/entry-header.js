8d96c3fcc1c1e501b02d235eff90567c99c1d2f5
bignetbrands <135934264+bignetbrands@users.noreply.github.com>
2023-06-07 23:56:37 -0700

    Update entry-header.js

--- components/entry-header.js
+++ components/entry-header.js
@@ -1,10 +1,9 @@
 import style from "./entry-header.module.css";
-import style from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
 
 export default function EntryHeader({ title, date, author }) {
   return (
     <div className={style.entry}>
-      {title && <h2 class="text-danger" className={style.title}>{title}</h2>}
+      {title && <h2 className={style.title}>{title}</h2>}
 
       {date && author && (
         <div className={style.meta}>
